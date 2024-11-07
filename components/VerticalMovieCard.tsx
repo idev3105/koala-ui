97a0c6498a2084b2b004d6f58eae8fb31bc8e514
idev3105 <khanhcongpruj123@gmail.com>
2024-11-07 23:10:55 +0700

    🗑️ Remove trailer overlay

--- components/VerticalMovieCard.tsx
+++ components/VerticalMovieCard.tsx
@@ -9,45 +9,17 @@ type VerticalMovieCardProps = {
   title?: string
   thumbUrl?: string
   rate?: number
-  trailerUrl?: string
   categories?: string[]
   onClickPlay?: () => void
   onClickBookmark?: () => void
   description?: string
 } & React.HTMLAttributes<HTMLDivElement>
 
-function TrailerOverlay({ trailerUrl, isVisible }: { trailerUrl: string; isVisible: boolean }) {
-  if (!isVisible) return null
-
-  return (
-    <div className="absolute inset-0 z-10 flex items-center justify-center bg-black bg-opacity-75">
-      <video src={trailerUrl} autoPlay loop muted className="h-full w-full object-cover" />
-    </div>
-  )
-}
-
-function DescriptionOverlay({
-  description,
-  isVisible,
-}: {
-  description: string
-  isVisible: boolean
-}) {
-  if (!isVisible) return null
-
-  return (
-    <div className="absolute left-0 right-0 top-full z-30 flex transform flex-col justify-end bg-black bg-opacity-75 p-4">
-      <p className="text-sm text-white">{description}</p>
-    </div>
-  )
-}
-
 export function VerticalMovieCard({
   className,
   title,
   thumbUrl,
   rate,
-  trailerUrl,
   categories,
   onClick,
   onClickPlay,
@@ -55,17 +27,12 @@ export function VerticalMovieCard({
   description,
 }: VerticalMovieCardProps) {
   const [imageError, setImageError] = useState(false)
-  const [isHovering, setIsHovering] = useState(false)
 
   return (
     <div
       className={`vertical-movie-card group relative h-full w-full rounded-md ${className} cursor-pointer`}
       onClick={onClick}
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
     >
-      {trailerUrl && <TrailerOverlay trailerUrl={trailerUrl} isVisible={isHovering} />}
-      <DescriptionOverlay description={description || ''} isVisible={isHovering} />
       <div className="relative top-0 h-full">
         {!imageError && (
           <Image
@@ -96,7 +63,7 @@ export function VerticalMovieCard({
           </div>
         </div>
       </div>
-      {/* <div className="absolute left-1/2 top-1/2 hidden h-fit w-fit -translate-x-1/3 -translate-y-1/2 items-center justify-center gap-2 group-hover:sm:inline-flex">
+      <div className="absolute left-1/2 top-1/2 hidden h-fit w-fit -translate-x-1/3 -translate-y-1/2 items-center justify-center gap-2 group-hover:sm:inline-flex">
         <button
           className="btn size-fit rounded-full border-none bg-gray-900/80 p-4"
           onClick={() => onClickPlay && onClickPlay()}
@@ -109,7 +76,7 @@ export function VerticalMovieCard({
         >
           <BookmarkIcon className="size-4" />
         </button>
-      </div> */}
+      </div>
     </div>
   )
 }
